30d88b2c988cadd4cb563c02161a1751882d07e7
ljy1020 <15050593078@139.com>
2019-09-14 11:24:57 +0800

    TEST

--- wetherui/config/index.js
+++ wetherui/config/index.js
@@ -10,7 +10,16 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {  //使用"/api"来代替"http://v.juhe.cn/toutiao/index"
+        target: 'http://istimeless.com/weather-info/weatherLive', //源地址
+        changeOrigin: true, //改变源
+        pathRewrite: {
+          '^/api': '' //路径重写
+        }
+      }
+
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
---
+++ wetherui/src/components/FirstPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div>
+      <span class="el-icon-location-outline"></span>
+      <span>{{nowCity}}</span>
+    </div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+    export default {
+        name: "FirstPage",
+        data(){
+          return {
+            nowCity:"雨花区"
+          }
+        },
+      mounted:function(){
+        this.getCityWeather();
+      },
+      methods:{
+        getCityWeather(){
+          axios.get("/api")
+            .then((result)=>{
+              var res = result.data;
+              alert("ok");
+             })
+            .catch(function (error) { // 请求失败处理
+              console.log(error);
+            });
+        }
+      }
+    }
+</script>
+<style>
+  .container{
+    width: 1000px;
+    margin: 0px auto;
+    background-color:#e4e5e6;
+  }
+</style>
+
