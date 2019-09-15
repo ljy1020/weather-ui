fc295e49a59e736f451a72a03f112ccedaef0bbd
SereneSoul <istimeless@foxmail.com>
2019-09-15 12:03:59 +0800

    init

--- wetherui/src/components/FirstPage.vue
+++ wetherui/src/components/FirstPage.vue
@@ -1,46 +1,49 @@
 <template>
-  <div class="container">
-    <!--搜索-->
-    <div class="searchBox">
-     <!-- <el-input v-model="searchCityName" placeholder="请输入城市"></el-input>
-      <el-button icon="el-icon-search" circle @click="searchWeather(searchCityName)"></el-button>
-    -->
-      <el-input
-        placeholder="请输入城市"
-        suffix-icon="el-icon-search"
-        v-model="searchCityName">
-      </el-input>
-      <div class="searchContent" v-show="searchContent">
-        sddsd
+  <div class="bodyBox">
+    <div class="container">
+      <!--搜索-->
+      <div class="searchBox">
+      </div>
+      <div class="location">
+        <span class="el-icon-location-outline"></span>
+        <em>{{weatherData.city}},{{weatherData.province}} </em>
+        <span class="el-icon-search" @click="showSearch = true"></span>
+        <span v-show="showSearch">
+        <input type="text" class="searchInput" v-model="searchCityName"
+               @input="searchCity(searchCityName)">
+        <span class="el-icon-close" @click="showSearch = false"></span>
+        <el-card class="box-card searchContent"  v-show="searchContent">
+          <div v-for="con in searchContent"  class="text item">
+           <span @click="getCityWeather(con.adcode)">{{con.name}}</span>
+          </div>
+      </el-card>
+      </span>
+
+      </div>
+      <div>
+        <span>{{weatherData.temperature}}</span>
+        <span>{{weatherData.weather}}</span>
+      </div>
+      <div>
+        <span>湿度 {{weatherData.humidity}}</span>
+        <span>{{weatherData.winddirection}}风</span>
+        <span>{{weatherData.windpower}}级</span>
+      </div>
+      <!--预报天气-->
+      <div class="forebox">
+        <table>
+          <tr v-for="data in foreWeatherData">
+            <td>{{data.date}}</td>
+            <td>{{data.dayweather}}</td>
+            <td>{{data.nighttemp}}/{{data.daytemp}}</td>
+            <td>{{data.daywind}}风</td>
+            <td>{{data.nightpower}}级</td>
+          </tr>
+        </table>
       </div>
     </div>
-    <div>
-      <span class="el-icon-location-outline"></span>
-      <span>{{weatherData.city}}</span>
-      <span>{{weatherData.province}}</span>
-    </div>
-    <div>
-      <span>{{weatherData.temperature}}</span>
-      <span>{{weatherData.weather}}</span>
-    </div>
-    <div>
-      <span>湿度 {{weatherData.humidity}}</span>
-      <span>{{weatherData.winddirection}}风</span>
-      <span>{{weatherData.windpower}}级</span>
-    </div>
-    <div></div>
-    <el-dialog
-      title="提示"
-      :visible.sync="dialogVisible"
-      width="30%"
-      :before-close="handleClose">
-      <span>网络出错，请稍后重试！</span>
-      <span slot="footer" class="dialog-footer">
-    <el-button @click="dialogVisible = false">取 消</el-button>
-    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
-  </span>
-    </el-dialog>
   </div>
+
 </template>
 
 <script>
@@ -51,62 +54,102 @@
           return {
             dialogVisible: false,
             weatherData :{},
-            searchCityName : ""
+            searchCityName : "",
+            searchContent : null,
+            showSearch : false,
+            foreWeatherData : null
           }
         },
       mounted:function(){
-        this.getCityWeather();
+        this.getCityWeather(null);
       },
       methods:{
-        handleClose(done) {
+      /*  handleClose(done) {
           this.$confirm('确认关闭？')
             .then(_ => {
               done();
             })
             .catch(_ => {});
-         },
-        getCityWeather(){
-          axios.get("/api/weather-info/weatherLive")
+         },*/
+        getCityWeather(adcode){
+          //当天天气
+          let getLiveUrl = "/api/weather-info/weatherLive";
+          let getForeUrl = "/api/weather-info/weatherForecast";
+          if(adcode){
+            getLiveUrl = getLiveUrl + "?adcode=" + adcode;
+            getForeUrl = getForeUrl + "?adcode=" + adcode;
+            this.searchContent = null;
+            this.searchCityName = null;
+          }
+          axios.get(getLiveUrl)
             .then((result)=>{
-              var res = result.data;
+              let res = result.data;
               if('1' == res.code){
                 this.weatherData = res.data;
               }else{
-                this.dialogVisible = true;
+                //this.dialogVisible = true;
+                this.$alert('网络出错，请稍后重试！', '提示', {
+                  confirmButtonText: '确定'
+                });
               }
 
              })
             .catch(function (error) { // 请求失败处理
-              this.dialogVisible = true;
+              //this.dialogVisible = true;
+              this.$alert('网络出错，请稍后重试！', '提示', {
+                confirmButtonText: '确定'
+              });
               console.log(error);
             });
-        },
-        searchWeather(name){
-          debugger;
-          if('0' == name.length){
-            this.$alert('请输入查询城市', '提示', {
-              confirmButtonText: '确定'/*,
-              callback: action => {
-                this.$message({
-                  type: 'info',
-                  message: `action: ${ action }`
-                });
-              }*/
-            });
-          }
-          var getUrl = "/api/weather-city/getCityInfo?cityName=" + name;
-          axios.get(getUrl)
+
+          //预报天气
+          axios.get(getForeUrl)
             .then((result)=>{
-              var res = result.data;
-              /*if('1' == res.code){
-                this.weatherData = res.data;
+              let res = result.data;
+              if('1' == res.code){
+                this.foreWeatherData = res.data.casts;
               }else{
-                this.dialogVisible = true;
-              }*/
+                this.$alert('网络出错，请稍后重试！', '提示', {
+                  confirmButtonText: '确定'
+                });
+              }
 
             })
             .catch(function (error) { // 请求失败处理
-              this.dialogVisible = true;
+              //this.dialogVisible = true;
+              this.$alert('网络出错，请稍后重试！', '提示', {
+                confirmButtonText: '确定'
+              });
+              console.log(error);
+            });
+        },
+        searchCity(name){
+          debugger;
+          if('0' == name.length){
+            return;
+          }
+          let getUrl = "/api/weather-city/getCityInfo?cityName=" + name;
+          axios.get(getUrl)
+            .then((result)=>{
+              let res = result.data;
+              if('1' == res.code){
+                if (res.data.length>0){
+                  this.searchContent = res.data;
+                }else{
+                  this.searchContent = null;
+                }
+
+              }else{
+                this.$alert('网络出错，请稍后重试！', '提示', {
+                  confirmButtonText: '确定'
+                });
+              }
+
+            })
+            .catch(function (error) { // 请求失败处理
+              this.$alert('网络出错，请稍后重试！', '提示', {
+                confirmButtonText: '确定'
+              });
               console.log(error);
             });
         }
@@ -114,19 +157,72 @@
     }
 </script>
 <style>
+  .bodyBox{
+    background: url("../assets/images/day_1.jpg") no-repeat;
+    background-size:cover;
+  }
   .container{
+    height: 1519px;
     width: 1000px;
     margin: 0px auto;
-    background-color:#e4e5e6;
+    padding: 10px 20px;
+    color: white;
   }
   .searchBox{
     width: 60%;
     position: relative;
+    margin: 0 auto;
   }
   .searchContent{
     position: absolute;
-    top: 40px;
+    top: 50px;
+    left : 420px;
+/*
     background-color: aliceblue;
+*/
+    background:  rgba(0, 0, 0, .3);
+    border:none;
+    color: white;
+  }
+  .location em {
+    font-weight: normal;
+    white-space: nowrap;；
+    overflow: hidden;
+    font-size: 20px;
+    line-height: 26px;
+    height: 26px;
+    font-style: normal;
+    padding-right: 10px;
+  }
+  .searchInput{
+   /* opacity: 1;*/
+    width: 140px;
+    overflow: hidden;
+    height: 25px;
+    line-height: 25px;
+    padding: 0;
+    border-bottom: 1px solid rgba(255, 255, 255, .2);
+    border-width: 0 0 1px 0;
+    outline: none;
+    background: none;
+    margin: 0 0 0 10px;
+    font-size: 14px;
+    color: #fff;
+  }
+  /**/
+  .forebox{
+    background:  rgba(0, 0, 0, .3);
+    border:none;
+    width: 600px;
+    margin: 20px 0px;
+    padding: 10px;
+  }
+  .forebox table{
+    width: 100%;
+  }
+  .forebox td{
+    width: 20%;
+    text-align: center;
   }
 </style>
 
